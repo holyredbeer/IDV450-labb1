04ddb60a1b335b431d0ca0393752beac493f4ab4
Henrik Petersson <henrikpetersson81@gmail.com>
2013-01-30 23:16:45 +0100

    Laddade upp projektfilerna

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ampta::Application.config.secret_token = '54078b3f727312451fe699f07d4cfe41747b4854e01864f833c52d20879afc3a2d69a9fda7fbc4f47907d50ed566db184af720f9b7879bafb2cbca61e59382db'
