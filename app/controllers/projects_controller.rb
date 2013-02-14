52600540f0e3c7f922307db3976236a4e040adda
Henrik Petersson <henrikpetersson81@gmail.com>
2013-02-14 15:53:49 +0100

    Sista fixarna...

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,10 +1,53 @@
 class ProjectsController < ApplicationController
 
-	before_filter :is_logged_in, :except => [:index, :show]
+	before_filter :is_logged_in, :except => [:index]
+	before_filter :check_if_owner_or_member, :only => [:show]
+	before_filter :check_if_owner, :only => [:edit, :update, :destroy]
+
+	def is_owner?
+	  Project.exists?(id: params[:id], user_id: current_user.id)
+	end
+
+	def is_member?
+	  ProjectsUser.exists?(project_id: params[:id], user_id: current_user.id)
+	end
+
+	def check_if_owner_or_member
+	  unless is_owner? || is_member?
+	    redirect_to root_path
+	    flash[:error] = "You don't have permission to the project!"
+	  end
+	end
+
+  	def check_if_owner
+  		unless is_owner?
+  			redirect_to root_path
+	      	flash[:error] = "You don't have permission to the project!"
+  		end
+  	end
 
 	def index
-	  @projects = Project.all
-	  # @user_is_member = ProjectsUser.where(:user_id => current_user.id) d
+
+	  if params[:filter] == "admin"
+
+	  	@projects = Project.where(:user_id => current_user.id)
+
+
+	  elsif params[:filter] == "member"
+	  	 @user_is_member = ProjectsUser.where(:user_id => current_user.id)
+
+		 project_ids = []
+
+		 @user_is_member.each do |member|
+		   project_ids << member.project_id
+		 end
+
+		 @projects = Project.where(:id => project_ids)
+
+	  else
+	    @projects = Project.all
+	   end
+
 	end
 
 	def new
@@ -42,7 +85,7 @@ class ProjectsController < ApplicationController
 		if @project.update_attributes(params[:project])
 
 			redirect_to manage_projects_path
-			flash.now[:success] = 'Projektet redigerades.' # Not quite right!
+			flash.now[:success] = 'The project was successfully edited.' # Not quite right!
 		else
 			render :action => "edit"
 		end
@@ -64,10 +107,10 @@ class ProjectsController < ApplicationController
 	  if @project = Project.find(params[:id])
 	  	@project.destroy
 	  	redirect_to manage_projects_path
-	  	flash.now[:success] = 'Projektet togs bort' # Not quite right!
+	  	flash.now[:success] = 'The project was deleted.' # Not quite right!
 	  else
 	  	redirect_to users_projects_path
-	  	flash.now[:error] = 'Nagot gick snett. Projektet kunde inte hittas.'
+	  	flash.now[:error] = "Something went wrong. The project could't be found."
 	  end
 	end
 end
